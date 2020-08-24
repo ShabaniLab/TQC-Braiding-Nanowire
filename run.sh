8314bc490116d5514c5b6e095abf0b8bdcd78e06
Arahant <arahant.ashok@gmail.com>
2020-08-24 17:12:21 -0400

    [master] running

--- run.sh
+++ run.sh
@@ -11,15 +11,11 @@ file_nanowire_states="nanowire-states.csv"
 file_tqc_metrics="tqc-metrics.csv"
 
 # Constructing the Adjacency Matrix for the given Nanowire structure
-# : > $file_nanowire_vertex
-# : > $file_nanowire_matrix
-# python nanowire-graph.py $file_nanowire_str $file_nanowire_vertex $file_nanowire_matrix
+: > $file_nanowire_vertex
+: > $file_nanowire_matrix
+python nanowire-graph.py $file_nanowire_str $file_nanowire_vertex $file_nanowire_matrix
 
 # TQC - performing braiding on the Nanowire
 echo "Particle,Path" > $file_particle_movement
 echo "P1,P2,P3,P4,P5,P6" > $file_nanowire_states
 python compiler.py $file_nanowire_str $file_nanowire_vertex $file_nanowire_matrix $file_braid_sequence $file_particle_position $file_particle_movement $file_nanowire_states
-
-# Calculating metrics
-# : > $file_tqc_metrics
-# python metrics.py $file_particle_movement $file_nanowire_states $file_tqc_metrics
